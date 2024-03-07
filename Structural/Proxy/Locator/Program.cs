4c207752d01a331cc85bd4963aa0ddaaea43d254
Nabi Karampour <thisisnabi@outlook.com>
2024-03-07 14:36:03 +0330

    Add indentation  into the program.cs locator service

--- Structural/Proxy/Locator/Program.cs
+++ Structural/Proxy/Locator/Program.cs
@@ -1,16 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.ConfigureServiceDbContext(builder.Configuration);
-builder.Services.ConfigureLocatorServices(builder.Configuration);
-builder.Services.ConfigureMapper();
-builder.Services.ConfigureAppSettings(builder.Configuration);
+{
+    builder.Services.ConfigureServiceDbContext(builder.Configuration);
+    builder.Services.ConfigureLocatorServices(builder.Configuration);
+    builder.Services.ConfigureMapper();
+    builder.Services.ConfigureAppSettings(builder.Configuration);
+}
 
 var app = builder.Build();
-
-app.MapGet("/locations/{ip:required}", 
-    (LocationFinder locationFinder,string ip, CancellationToken cancellationToken) =>
-        locationFinder.GetAsync(ip, cancellationToken));
-
+{
+    app.MapGet("/locations/{ip:required}",
+        (LocationFinder locationFinder, string ip, CancellationToken cancellationToken) =>
+            locationFinder.GetAsync(ip, cancellationToken));
+}
 app.Run();
 
 
