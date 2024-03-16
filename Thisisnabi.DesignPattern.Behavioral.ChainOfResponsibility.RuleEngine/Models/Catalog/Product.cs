df9a3e46ab5b5722f8b6f94615b3374c1ed55a9f
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-16 21:16:58 +0330

    feat: Add ProductEntity

---
+++ Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine/Models/Catalog/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine.Models.Catalog;
+
+public sealed class Product
+{
+    #region Ctors
+    private Product(string name, string category, int score = 0)
+    {
+        this.Name = name;
+        this.Category = category;
+        this.Score = score;
+    }
+    public Product CreateProductWithOutScore(string name, string category)
+        => new Product(name: name, category: category);
+    public Product CreateProduct(string name, string category, int score)
+        => new Product(name: name, category: category, score: score);
+    #endregion
+    #region Properties
+    public string Name { get; private set; }
+    public string Category { get; private set; }
+    public int Score { get; private set; } = 0;
+    #endregion
+    #region Methods
+    public void SetName(string name)
+        => this.Name = name;
+    public void SetCategory(string category)
+        => this.Category = category;
+    public void SetScore(int score)
+        => this.Score = score;
+    #endregion
+}
