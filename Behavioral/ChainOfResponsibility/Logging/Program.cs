c09e26f72e4639e9548b80c5460ce8d089da2f90
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-16 23:10:22 +0330

    style: undo Logging To Main Branch

--- Behavioral/ChainOfResponsibility/Logging/Program.cs
+++ Behavioral/ChainOfResponsibility/Logging/Program.cs
@@ -12,5 +12,3 @@ fileLogger.SetNextLogger(emailLogger);
 consoleLogger.LogMessage(LogLevel.Information, "This is an information.");
 consoleLogger.LogMessage(LogLevel.Warning, "This is a warning.");
 consoleLogger.LogMessage(LogLevel.Error, "This is an error.");
-
-Console.Read();
