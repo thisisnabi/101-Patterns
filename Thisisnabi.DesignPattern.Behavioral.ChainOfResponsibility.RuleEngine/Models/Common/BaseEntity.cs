230afbcffe1311cc822980b556face5834eb772a
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-16 21:11:00 +0330

    feat: BaseEntity

---
+++ Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine/Models/Common/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine.Models.Common
+{
+    public class BaseEntity
+    {
+        #region Properties
+        public Guid Id { get; set; }
+        public DateTime InsertDate { get; private set; } = DateTime.Now;
+        public bool IsDeleted { get; private set; } = false;
+        public DateTime? DeleteDate { get; private set; }
+        #endregion
+        #region Methods
+        public void SetDeleteAction()
+        {
+            this.IsDeleted = true;
+            this.DeleteDate = DateTime.Now;
+        }
+             
+        #endregion
+    }
+}
